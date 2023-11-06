ec59fba6fcf0e5d8b22ab1126dba8ee02451450d
sz3papdav <144676937+sz3papdav@users.noreply.github.com>
2023-11-06 11:20:22 +0100

    igen
    igen

---
+++ javito.js
@@ -0,0 +1,5 @@
+document.getElementById("container").style.backgroundColor="#12406A"
+document.querySelectorAll(".div")[0].style.backgroundColor="#EB8F90"
+document.querySelectorAll(".div")[1].style.backgroundColor="#FFB471"
+document.querySelectorAll(".div")[2].style.backgroundColor="#ADBED2"
+document.body.style.backgroundColor="#CCFFFF"
